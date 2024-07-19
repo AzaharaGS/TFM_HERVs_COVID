2343269a6db64236bef03cea2a6c2e1af9fc2a7a
AzaharaGS <121137602+AzaharaGS@users.noreply.github.com>
2024-07-19 21:43:50 +0200

    Delete TFM_merge.R

--- TFM_merge.R
+++
@@ -1,5 +0,0 @@
-##Combinamos la portada y la memoria
-library(qpdf)
-
-qpdf::pdf_combine(input = c("TFM_AGS_frontpage.pdf", "TFM_memoria_AMGS.pdf"),
-                  output = "TFM_AMGS_Bioinformatica_24.pdf")
